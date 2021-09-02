1f0f495f1c2e7dd05586e4ac1e8d19c9d4301de5
blainedeyoung <45204685+blainedeyoung@users.noreply.github.com>
2021-09-01 20:02:15 -0400

    firebase set up

--- client/src/routes/auth/Register.jsx
+++ client/src/routes/auth/Register.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config = {
+      url: "http://localhost:3000/register/complete",
+      handleCodeInApp: true,
+    };
+    await auth.sendSignInLinkToEmail(email, config);
+    toast.success(
+      `Email sent to ${email}.  Click the link to complete registration.`
+    );
+    window.localStorage.setItem("emailForRegistration", email);
+    setEmail("");
+  };
+
+  const registrationForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+      <button type="submit" className="btn btn-primary btn-raised mt-2">
+        Register
+      </button>
+    </form>
+  );
+
   return (
-    <div>
-      <p>Register</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registrationForm()}
+        </div>
+      </div>
     </div>
   );
 };
